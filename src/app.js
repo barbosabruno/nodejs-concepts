bb1181f837f118268956fcb88120d19c9ef3593d
Bruno Barbosa <bruno.barbosa.cs@gmail.com>
2020-09-09 21:44:24 -0300

    Node.js concepts

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { v4: uuid } = require('uuid');
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +10,88 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  
+  const { title, url, techs } = request.body;
+
+  const repo = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  };
+
+  repositories.push(repo);
+
+  return response.json(repo);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+  
+  const index = findIndex(repositories, id);
+  if (!repositoryExists(index)) {
+    return response.status(400).json();
+  }
+
+  let repo = repositories[index];
+  
+  repo = {
+    ...repo,
+    title,
+    url,
+    techs
+  };
+
+  repositories.splice(index, 1, repo);
+
+  return response.json(repo);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+
+  const { id } = request.params;
+  const index = findIndex(repositories, id);
+
+  if (!repositoryExists(index)) {
+    return response.status(400).json();
+  }
+  
+  repositories.splice(index, 1);
+  
+  return response.status(204).json();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  
+  const { id } = request.params;
+  const index = findIndex(repositories, id);
+
+  if (!repositoryExists(index)) {
+    return response.status(400).json();
+  }
+
+  let repo = repositories[index];
+
+  repo = {
+    ...repo,
+    likes: repo.likes + 1
+  }
+
+  repositories.splice(index, 1, repo);
+
+  return response.json(repo);
 });
+
+const findIndex = (repositories, id) => 
+  repositories.findIndex(repo => repo.id === id);
+
+const repositoryExists = index => index > -1;
 
 module.exports = app;
